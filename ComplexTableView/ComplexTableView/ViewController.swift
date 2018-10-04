39063972041e0a814b6003c414f2f5d654c734bc
Alina Zaitseva <zaitseva.alina@steelkiwi.com>
2018-10-04 09:52:20 +0300

    added project

---
+++ ComplexTableView/ComplexTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComplexTableView
+//
+//  Created by Alina Zaitseva on 10/4/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
