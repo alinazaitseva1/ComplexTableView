c188e2533debd5dc5a3b61f03f4553c296f984f9
Alina Zaitseva <zaitseva.alina@steelkiwi.com>
2018-10-04 12:36:57 +0300

    added view controller layout

---
+++ ComplexTableView/ComplexTableView/Views/ContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContentTableViewCell.swift
+//  ComplexTableView
+//
+//  Created by Alina Zaitseva on 10/4/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class ContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
