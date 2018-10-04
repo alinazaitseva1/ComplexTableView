8dabe07a9fc71a42cf3143b3348d9c5bae518e43
Alina Zaitseva <zaitseva.alina@steelkiwi.com>
2018-10-04 12:13:41 +0300

    added view controller layout

--- ComplexTableView/ComplexTableView/ViewControllers/ViewController.swift
+++ ComplexTableView/ComplexTableView/ViewControllers/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
